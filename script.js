527f81d26fd5081699fdca1fa8252c1b1220dd17
DaphneFlapper <daphne.flapper.ap@gmail.com>
2020-02-08 11:52:57 +0100

    1.0

---
+++ script.js
@@ -0,0 +1,85 @@
+
+var iso = new Isotope( '.grid', {
+  itemSelector: '.element-item',
+  layoutMode: 'fitRows'
+});
+
+// filter functions
+var filterFns = {
+  // show if number is greater than 50
+  numberGreaterThan50: function( itemElem ) {
+    var number = itemElem.querySelector('.number').textContent;
+    return parseInt( number, 10 ) > 50;
+  },
+  // show if name ends with -ium
+  ium: function( itemElem ) {
+    var name = itemElem.querySelector('.name').textContent;
+    return name.match( /ium$/ );
+  }
+};
+
+// bind filter button click
+var filtersElem = document.querySelector('.filters-button-group');
+filtersElem.addEventListener( 'click', function( event ) {
+  // only work with buttons
+  if ( !matchesSelector( event.target, 'button' ) ) {
+    return;
+  }
+  var filterValue = event.target.getAttribute('data-filter');
+  // use matching filter function
+  filterValue = filterFns[ filterValue ] || filterValue;
+  iso.arrange({ filter: filterValue });
+});
+
+// change is-checked class on buttons
+var buttonGroups = document.querySelectorAll('.button-group');
+for ( var i=0, len = buttonGroups.length; i < len; i++ ) {
+  var buttonGroup = buttonGroups[i];
+  radioButtonGroup( buttonGroup );
+}
+
+function radioButtonGroup( buttonGroup ) {
+  buttonGroup.addEventListener( 'click', function( event ) {
+    // only work with buttons
+    if ( !matchesSelector( event.target, 'button' ) ) {
+      return;
+    }
+    buttonGroup.querySelector('.is-checked').classList.remove('is-checked');
+    event.target.classList.add('is-checked');
+  });
+}
+
+//sorting
+var iso2 = new Isotope( '.grid', {
+  itemSelector: '.element-item',
+  layoutMode: 'fitRows',
+  getSortData: {
+  prijs: '.prijs parseInt',
+  }
+});
+
+// bind sort button click
+var sortByGroup = document.querySelector('.sort-by-button-group');
+sortByGroup.addEventListener( 'click', function( event ) {
+  // only button clicks
+  if ( !matchesSelector( event.target, '.button' ) ) {
+    return;
+  }
+  var sortValue = event.target.getAttribute('data-sort-value');
+  iso2.arrange({ sortBy: sortValue });
+});
+
+// change is-checked class on buttons
+var buttonGroups = document.querySelectorAll('.button-group');
+for ( var i=0; i < buttonGroups.length; i++ ) {
+  buttonGroups[i].addEventListener( 'click', onButtonGroupClick );
+}
+
+function onButtonGroupClick( event ) {
+  // only button clicks
+  if ( !matchesSelector( event.target, '.button' ) ) {
+    return;
+  }
+  var button = event.target;
+
+}
